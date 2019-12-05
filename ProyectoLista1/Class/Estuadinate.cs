aeb8768cc23526447c5c1afaa7cdd239ed96f7eb
jhourodriguez5701 <jhousaso@gmail.com>
2019-12-05 10:32:43 -0500

    Agregar archivos de proyecto.

---
+++ ProyectoLista1/Class/Estuadinate.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoLista1.Class
+{
+    class Estuadinate
+    {
+       private String NombreCompleto { get; set; }
+        private String Genero { get; set; }
+        private String EstadoCivil { get; set; }
+        private int edad { get; set; }
+    }
+}
